fe38a194f04b4a2c3db3473686f1b0257c25cb56
Rana Hesham <rhesham162@gmail.com>
2025-12-15 08:18:29 +0200

    feat: implement real-time friend request notifications

--- src/friend/friend.gateway.ts
+++ src/friend/friend.gateway.ts
@@ -1,5 +1,11 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { ResponseFriendRequestDto } from './dto/response-request-friend.dto';
 
 @WebSocketGateway({
@@ -11,13 +17,18 @@ export class FriendGateway {
   @WebSocketServer()
   server: Server; //===io
 
+  @SubscribeMessage('join_room')
+  async handleJoinRoom(
+    @MessageBody() userId: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    await client.join(userId);
+  }
+
   handleSentFriendRequest(
     recieverId: string,
     friendRequest: ResponseFriendRequestDto,
   ) {
     this.server.to(recieverId).emit('sent-friend-request', friendRequest);
   }
-  // handleNewComment(comment: ResponseCommentDto) {
-  //   this.server.emit('new_comment', comment);
-  // }
 }
